32fc9dcd206787c064d726d3dc3bedc261f26a96
Ederson Machado <edersonmberti@gmail.com>
2020-05-14 19:21:22 -0300

    added state controller

---
+++ src/controllers/StateController.js
@@ -0,0 +1,24 @@
+const State = require("../models/StateModel");
+
+exports.show = async (request, response) => {
+  const { page = 1, limit = 27 } = request.query;
+
+  const states = await State.paginate(
+    {},
+    {
+      page,
+      limit,
+      select: "-createdAt -updatedAt -__v -cities -_id",
+    }
+  );
+
+  response.status(200).send({ states });
+};
+
+exports.index = async (request, response) => {
+  const { id } = request.params;
+
+  const state = await State.findOne({ id });
+
+  response.status(200).send({ state });
+};
